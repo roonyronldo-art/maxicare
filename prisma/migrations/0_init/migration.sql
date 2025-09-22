b5e3b8f99414b2d54278f8d7c67cfe9d5496abc8
our ame <you@example.com>
2025-09-22 15:06:17 -0700

    Add initial Prisma migration

---
+++ prisma/migrations/0_init/migration.sql
@@ -0,0 +1,111 @@
+﻿-- CreateSchema
+CREATE SCHEMA IF NOT EXISTS "public";
+
+-- CreateTable
+CREATE TABLE "public"."LabUser" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "age" INTEGER,
+    "address" TEXT,
+    "phone" TEXT,
+    "name" TEXT,
+    "role" TEXT NOT NULL DEFAULT 'user',
+
+    CONSTRAINT "LabUser_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."ClinicConfig" (
+    "id" INTEGER NOT NULL DEFAULT 1,
+    "heroImg" TEXT,
+    "loginImg" TEXT,
+    "registerImg" TEXT,
+
+    CONSTRAINT "ClinicConfig_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Schedule" (
+    "id" SERIAL NOT NULL,
+    "dayOfWeek" INTEGER NOT NULL,
+    "startHour" INTEGER NOT NULL,
+    "endHour" INTEGER NOT NULL,
+    "slotMinutes" INTEGER NOT NULL DEFAULT 30,
+
+    CONSTRAINT "Schedule_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Appointment" (
+    "id" SERIAL NOT NULL,
+    "start" TIMESTAMP(3) NOT NULL,
+    "status" TEXT NOT NULL DEFAULT 'pending',
+    "scheduleId" INTEGER NOT NULL,
+    "report" TEXT,
+    "userId" TEXT,
+    "attachment" TEXT,
+    "created" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Appointment_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Ticket" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "status" TEXT NOT NULL DEFAULT 'open',
+    "created" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "Ticket_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."TicketAttachment" (
+    "id" SERIAL NOT NULL,
+    "url" TEXT NOT NULL,
+    "ticketId" INTEGER NOT NULL,
+    "uploaded" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "TicketAttachment_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."LabRequest" (
+    "id" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "status" TEXT DEFAULT 'pending',
+    "reply" TEXT,
+    "attachments" TEXT,
+    "created" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "LabRequest_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "LabUser_email_key" ON "public"."LabUser"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Schedule_dayOfWeek_key" ON "public"."Schedule"("dayOfWeek");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Appointment_start_key" ON "public"."Appointment"("start");
+
+-- AddForeignKey
+ALTER TABLE "public"."Appointment" ADD CONSTRAINT "Appointment_scheduleId_fkey" FOREIGN KEY ("scheduleId") REFERENCES "public"."Schedule"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Appointment" ADD CONSTRAINT "Appointment_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."LabUser"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Ticket" ADD CONSTRAINT "Ticket_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."LabUser"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."TicketAttachment" ADD CONSTRAINT "TicketAttachment_ticketId_fkey" FOREIGN KEY ("ticketId") REFERENCES "public"."Ticket"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."LabRequest" ADD CONSTRAINT "LabRequest_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."LabUser"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
